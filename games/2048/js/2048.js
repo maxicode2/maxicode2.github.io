a639f729e80b5d5a9dc085dab79c12906af9e2cb
maxicode2 <65052855+maxicode2@users.noreply.github.com>
2022-03-21 20:19:25 -0700

    Delete games/2048 directory

--- games/2048/js/2048.js
+++
@@ -1,7 +0,0 @@
-let trig = document.getElementById("bean-credits-trigger")
-let cred = trig.parentElement
-cred.setAttribute("state", "hidden")
-trig.onclick = () => {
-    if(cred.getAttribute("state") === "hidden") cred.setAttribute("state", "revealed")
-    else cred.setAttribute("state", "hidden")
-}
