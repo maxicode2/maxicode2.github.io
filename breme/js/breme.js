5a15091502cdf6e5559f3b27a2982d418cf283da
Maximus Zhang <zhang.maximus@gmail.com>
2022-05-30 21:05:34 -0700

    breme++ (yt didnt work)

--- breme/js/breme.js
+++ breme/js/breme.js
@@ -1,13 +1,10 @@
-document.getElementById("get-perms").onclick = () => {
-    let vid = document.createElement("iframe");
-    vid.id = "vid"
-    vid.src = "https://www.youtube.com/embed/DLzxrzFCyOs?autoplay=1&controls=0&modestbranding&rel=0&loop=1&disablekb=1&fs=0&iv_load_policy=3"
-    vid.frameBorder = 0;
-    vid.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-    vid.allowFullscreen = true;
+let audio = new Audio("media/rick.mp3")
 
-    console.log("a")
-    document.getElementById("breme").appendChild(vid)
-    document.getElementById("addition").style.display = "flex"
-    document.getElementById("get-perms").remove()
+document.getElementById("get-perms").onclick = () => {
+    audio.play()
+    audio.onplaying = () => {
+        document.getElementById("addition").style.display = "flex"
+        document.getElementById("rage").style.display = "block"
+        document.getElementById("get-perms").remove()
+    }
 }
